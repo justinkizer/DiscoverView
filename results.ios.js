e0615a20f561b832613551d018c583ed27a0d27a
James Stack <jstack@Jamess-MacBook-Pro.local>
2017-04-04 21:11:41 -0700

    Fix results view

--- results.ios.js
+++ results.ios.js
@@ -12,14 +12,24 @@ import {
 export default class Results extends React.Component {
   constructor(props){
     super(props);
-    this.state = {data: null, accessToken: '4991830679.e029fea.784582c3f64a46088b0b1f124469dd3d', lat: 40.745812, lng: -111.849493, dist: 1000 };
+    this.state = {dataSource: new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2,
+    }), loaded: false, accessToken: '4991830679.e029fea.784582c3f64a46088b0b1f124469dd3d', lat: 40.745812, lng: -111.849493, dist: 1000 };
   }
 
 
-  componentWillMount(){
-    fetch(`https://api.instagram.com/v1/media/search?lat=${this.state.lat}&lng=${this.state.lng}&distance=${this.state.dist}&access_token=${this.state.accessToken}`,
-        {method: "GET", dataType: 'json'}).then(function(response){ return response.json();})
-          .then(json => this.setState({data: json.data}));
+  componentDidMount(){
+    this.fetchData();
+  }
+
+  fetchData(){
+    fetch(`https://api.instagram.com/v1/media/search?lat=${this.state.lat}&lng=${this.state.lng}&distance=${this.state.dist}&access_token=${this.state.accessToken}`)
+        .then((response) => response.json())
+        .then((responseData) => {
+          this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(responseData.data)
+          });
+        });
   }
 
 
@@ -48,28 +58,54 @@ export default class Results extends React.Component {
 
 
   render() {
-    console.log(this.photoUrls()[0]);
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    console.log("should get here!");
     return (
-      <View style={styles.resultsContainer}>
-          {this.photosArray()}
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderPhoto}
+        style={styles.listview}
+        />
+    );
+  }
+
+  renderLoadingView(){
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading photos...
+        </Text>
+      </View>
+    );
+  }
+
+  renderPhoto(photo){
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: photo.images.standard_resolution.url}}
+          style={styles.photo}
+          />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  resultsContainer: {
+  container: {
     flex: 1,
     alignItems: "center",
     flexDirection: "column",
     backgroundColor: 'transparent'
   },
-  resultsBackground: {
-    flex: 1,
-    flexDirection: "column",
-    zIndex: -1
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF'
   },
-  image: {
+  photo: {
     width: 250,
     height: 250
 
