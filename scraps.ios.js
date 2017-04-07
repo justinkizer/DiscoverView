505666116f83bacbff1ad43bb40c41142fd85001
James Stack <jstack@Jamess-MacBook-Pro.local>
2017-04-06 22:30:51 -0700

    Clean up results

--- scraps.ios.js
+++ scraps.ios.js
@@ -1,4 +1,17 @@
-<Image
-  source={{uri: photo.images.standard_resolution.url}}
-  style={styles.photo}
-  />
+fetchMultipleLocationsMedia(idsArray){
+  console.log('got here at least!');
+  idsArray.forEach(id => {
+    this.fetchLocationMedia(id);
+  });
+  console.log(this.state.locationMedia);
+}
+
+fetchLocationMedia(id){
+  fetch(`https://api.instagram.com/v1/locations/${id}/media/recent?access_token=${this.state.accessToken}`)
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({
+        locationMedia: this.state.locationMedia.concat(responseData.data)
+      });
+    });
+}
